e2b5a886714bf38a330f4f1ceb54b322cbcf7e89
lizhi111 <43477849+lizhi111@users.noreply.github.com>
2018-10-16 00:54:16 +0800

    Update dataset.py

--- src/dataset.py
+++ src/dataset.py
@@ -1,20 +1,32 @@
 from src.core import Basic
 import networkx as nx
-
+import numpy as np
 
 class Dataset(Basic):
     def __init__(self, dataset_str):
         super().__init__()
+        self.graph = self.load_dataset(dataset_str)
+
+    def load_dataset(self,dataset_str):
+        g = nx.Graph()
+        # Load dataset
         if dataset_str == 'facebook':
-            self.graph = self.load_facebook_dataset()
+            edgelist = np.loadtxt(r'C:\Users\user\Desktop\CE7490-Group-Project-Python-L1\dataset\Facebook.txt',int)
+        elif dataset_str == 'twitter':
+            edgelist = np.loadtxt(r'C:\Users\user\Desktop\CE7490-Group-Project-Python-L1\dataset\Twitter.txt',int)
+        elif dataset_str == 'twitters1':
+            edgelist = np.loadtxt(r'C:\Users\user\Desktop\CE7490-Group-Project-Python-L1\dataset\TwittersSample1.txt',int)
+        elif dataset_str == 'twitters2':
+            edgelist = np.loadtxt(r'C:\Users\user\Desktop\CE7490-Group-Project-Python-L1\dataset\TwittersSample2.txt',int)
+        elif dataset_str == 'amazon':
+            edgelist = np.loadtxt(r'C:\Users\user\Desktop\CE7490-Group-Project-Python-L1\dataset\Amazon.txt',int)
+        elif dataset_str == 'amazons':
+            edgelist = np.loadtxt(r'C:\Users\user\Desktop\CE7490-Group-Project-Python-L1\dataset\AmazonSample.txt',int)
+        elif dataset_str == 'p2pgnutella':
+            edgelist = np.loadtxt(r'C:\Users\user\Desktop\CE7490-Group-Project-Python-L1\dataset\p2pGnutella.txt',int)
         else:
             pass
-
-    def load_facebook_dataset(self):
-        g = nx.Graph()
-
-        # Load dataset
-
+        g.add_edges_from(edgelist)
         return g
 
     def get_all_adj_node_id_list(self, node_id):
@@ -25,3 +37,4 @@ class Dataset(Basic):
             return True
         else:
             return False
+
