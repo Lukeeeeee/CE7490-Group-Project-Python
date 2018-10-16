28505af49e81a57b794389cfbe9c1e4dad2d2061
lizhi111 <43477849+lizhi111@users.noreply.github.com>
2018-10-17 00:41:29 +0800

    Add files via upload

---
+++ test/RandomTest.py
@@ -0,0 +1,34 @@
+from src.dataset import Dataset
+from src.server.server import Server
+from src.randomP.randomP import RandomP
+import random
+import time
+
+
+def main():
+    network_dataset = Dataset('amazon')
+    server_list = [Server(k) for k in range(0, 15)]
+    node_list=list(network_dataset.graph.nodes)
+    random.shuffle(node_list)
+    print('Dataset information:\nNodes Number:',network_dataset.graph.order(),'\nEdge Number:',
+          network_dataset.graph.size())
+    print('Using Random Partitioning Method...\nServer Number: 16\nVirtual Primary Copy Number: 3'
+          '\nWrite Frequency of Nodes: 1')
+    start=time.time()
+    m = RandomP(server_list, network_dataset,node_list)
+    m.add_new_primary_node()
+    m.check_server_load()
+    m.check_locality()
+    end=time.time()
+    print('Random Partitioning Time:',end-start,'seconds')
+    m.compute_inter_sever_cost()
+
+
+
+main()
+
+
+
+
+
+
