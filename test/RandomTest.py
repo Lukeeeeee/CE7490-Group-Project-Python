d08749cb088bd2f2cd171597fea1edc6082fd2d4
lizhi111 <43477849+lizhi111@users.noreply.github.com>
2018-10-24 13:25:06 +0800

    Update RandomTest.py

--- test/RandomTest.py
+++ test/RandomTest.py
@@ -6,17 +6,24 @@ import time
 
 
 def main():
-    network_dataset = Dataset('amazon')
-    server_list = [Server(k) for k in range(0, 15)]
+    network_dataset = Dataset('amazons')
+
+    #10% sampling
+    nbunch=[i for i in range(0,network_dataset.graph.order()//10)]
+    network_dataset.graph=network_dataset.graph.subgraph(nbunch)
+
+    server_list = [Server(k) for k in range(0, 127)]
+    vp_number = 3
+
     node_list=list(network_dataset.graph.nodes)
     random.shuffle(node_list)
     print('Dataset information:\nNodes Number:',network_dataset.graph.order(),'\nEdge Number:',
           network_dataset.graph.size())
-    print('Using Random Partitioning Method...\nServer Number: 16\nVirtual Primary Copy Number: 3'
+    print('Using Random Partitioning Method...\nServer Number: 128\nVirtual Primary Copy Number:',vp_number,
           '\nWrite Frequency of Nodes: 1')
     start=time.time()
     m = RandomP(server_list, network_dataset,node_list)
-    m.add_new_primary_node()
+    m.add_new_primary_node(server_list,vp_number)
     m.check_server_load()
     m.check_locality()
     end=time.time()
@@ -24,11 +31,4 @@ def main():
     m.compute_inter_sever_cost()
 
 
-
 main()
-
-
-
-
-
-
