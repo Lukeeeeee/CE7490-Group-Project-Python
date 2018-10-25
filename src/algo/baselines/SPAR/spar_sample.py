387052a986330e2adafefec79d63f1ac288b4612
plusmultiple <527441520@qq.com>
2018-10-25 21:53:55 +0800

    Spar experiment 1 with stop at 10 percent of the data

--- src/algo/baselines/SPAR/spar_sample.py
+++ src/algo/baselines/SPAR/spar_sample.py
@@ -8,6 +8,7 @@ from src.algo.baselines.SPAR.read_data import read_file_to_dict
 from src.algo.baselines.SPAR.Spar import Spar
 from dataset import DATASET_PATH
 from log import LOG_PATH
+from src.constant import Constant
 
 
 def save_obj(obj, name, path):
@@ -28,6 +29,15 @@ def spar_inter_server_cost(slave_dict):
     return cost
 
 
+def calculate_mean_length(dict):
+    length = 0
+    iter = 0
+    for key, data in dict.items():
+        length += len(data)
+        iter += 1
+    return int(length/iter)
+
+
 def test_spar_sample(file, server_number, minimum_replicas, percent10_stop=False):
     filename = DATASET_PATH + '/' + file + '.txt'
     # path = '././dataset/'
@@ -35,35 +45,56 @@ def test_spar_sample(file, server_number, minimum_replicas, percent10_stop=False
 
     node_list, node_neighbor_dic, col_data = read_file_to_dict(filename)
 
+    node_num = 0
     start_time = time.time()
     spar = Spar(server_number, minimum_replicas)
     if percent10_stop is True:
         for i in range(int(len(node_list) / 10)):
             n = node_list[i]
             spar.new_node(n, node_neighbor_dic[n])
+            node_num += 1
     else:
         for n in tqdm(node_list):
             spar.new_node(n, node_neighbor_dic[n])
+            node_num += 1
 
     running_time = time.time() - start_time
-    # master_node_to_server = spar.node_server_dic
-    # replicas_to_server = spar.replica_server_dic
+    master_node_to_server = spar.node_server_dic
+    replicas_to_server = spar.replica_server_dic
+    ave_replicas_of_nodes = calculate_mean_length(replicas_to_server)
+    graph = spar.G
+    num_edges = graph.number_of_edges()
 
     masters_in_each_server = spar.servers_master
     replicas_in_each_server = spar.servers_slave
+    ave_master_in_servers = calculate_mean_length(masters_in_each_server)
+    ave_slaves_in_servers = calculate_mean_length(replicas_in_each_server)
+    averages = [ave_replicas_of_nodes, ave_master_in_servers, ave_slaves_in_servers]
+    cost = spar_inter_server_cost(replicas_in_each_server)
+    print(file)
+    print('node'+str(node_num))
+    print('edge'+str(num_edges))
+    print('cost'+str(cost))
+    print('averages'+str(averages))
+    print('data_shape'+str(np.shape(col_data)))
 
     del spar
-    # graph = spar.G
 
-    return spar_inter_server_cost(replicas_in_each_server), running_time
+
+    return cost, running_time, averages, node_num, num_edges
 
 
 def spar_experiment_1(percent10_stop=False):
     # Experiment 1 inter server traffic cost
-    # digraph_files = ['AmazonSample', 'Amazon', 'Twitter', 'TwitterSample1', 'TwitterSample2', 'Facebook', 'p2pGnutella']
+
+
+
+    #digraph_files = ['AmazonSample', 'Amazon', 'Twitter', 'TwitterSample1', 'TwitterSample2', 'Facebook', 'p2pGnutella']
     digraph_files = ['AmazonSample', 'TwitterSample1', 'TwitterSample2', 'Facebook', 'p2pGnutella']
-    # digraph_files = ['AmazonSample']
-    num_server = 128
+    #digraph_files = ['Amazon', 'Twitter']
+    #digraph_files = ['AmazonSample']
+
+    num_server = Constant.SERVER_NUMBER
 
     # create logger
     # logger_name = "Spar_experiment1"
@@ -91,9 +122,12 @@ def spar_experiment_1(percent10_stop=False):
     minimum_replica = 2
     for file in digraph_files:
         print('Processing 2 replicas' + file)
-        cost, running_time = test_spar_sample(file, num_server, minimum_replica, percent10_stop)
-        logger.info('file %s, server number = %s, minimum replicas = %s, cost = %s, time = %s', file,
-                    str(num_server), str(minimum_replica), str(cost), str(running_time))
+        cost, running_time, averages, num_nodes, num_edges = test_spar_sample(file, num_server, minimum_replica,
+                                                                              percent10_stop)
+        logger.info('file %s, server number = %s, nodes = %s, edges = %s cost = %s, time = %s', file,
+                    str(num_server), str(num_nodes), str(num_edges), str(cost), str(running_time))
+        logger.info('average: replica_per_node=%s, master_per_server=%s, replicas_per_server=%s',
+                    str(averages[0]), str(averages[1]), str(averages[2]))
         replica_2_result[file] = cost
 
     print(replica_2_result)
@@ -103,9 +137,12 @@ def spar_experiment_1(percent10_stop=False):
     minimum_replica = 3
     for file in digraph_files:
         print('Processing 3 replicas' + file)
-        cost, running_time = test_spar_sample(file, num_server, minimum_replica, percent10_stop)
-        logger.info('file %s, server number = %s, minimum replicas = %s, cost = %s, time = %s', file,
-                    str(num_server), str(minimum_replica), str(cost), str(running_time))
+        cost, running_time, averages, num_nodes, num_edges = test_spar_sample(file, num_server, minimum_replica,
+                                                                              percent10_stop)
+        logger.info('file %s, server number = %s, nodes = %s, edges = %s cost = %s, time = %s', file,
+                    str(num_server), str(num_nodes), str(num_edges), str(cost), str(running_time))
+        logger.info('average: replica_per_node=%s, master_per_server=%s, replicas_per_server=%s',
+                    str(averages[0]), str(averages[1]), str(averages[2]))
         replica_3_result[file] = cost
 
     print(replica_3_result)
@@ -114,4 +151,4 @@ def spar_experiment_1(percent10_stop=False):
 
 
 if __name__ == '__main__':
-    spar_experiment_1(percent10_stop=True)
+    spar_experiment_1(percent10_stop=False)
