975c4276c4f31ee5e12988d5ed2d047cf7727fc9
lizhi111 <43477849+lizhi111@users.noreply.github.com>
2018-10-27 14:40:14 +0800

    Update RandomTest.py

--- test/RandomTest.py
+++ test/RandomTest.py
@@ -5,28 +5,28 @@ from dataset import DATASET_PATH
 import random
 import time
 import os
+from src.dataset import read_file_to_dict
+from graphs.random import RANDOM_GRAPH_PATH
 
-
-from src.algo.baselines.SPAR.read_data import read_file_digraph, read_file_ndgraph
 
 def main():
 
-    network_dataset = Dataset('facebook')
+    network_dataset = Dataset('twitters2')
 
-    nl, node_neighbor_dic, col_data = read_file_ndgraph(os.path.join(DATASET_PATH, 'Facebook.txt'))
+    nl = read_file_to_dict(os.path.join(DATASET_PATH, 'TwitterSample2.txt'))
 
     #10% sampling
-    nbunch = nl[0:(len(nl)//10)]
+    nbunch = nl[0:int(len(nl)//2)]
     network_dataset.graph=network_dataset.graph.subgraph(nbunch)
 
-    server_list = [Server(k) for k in range(0, 127)]
-    vp_number = 3
+    server_list = [Server(k) for k in range(0, 512)]
+    vp_number = 0
 
     node_list=list(network_dataset.graph.nodes)
     random.shuffle(node_list)
-    print('Dataset information:\nNodes Number:',network_dataset.graph.order(),'\nEdge Number:',
+    print('Dataset information: TwitterSample2\nNodes Number:',network_dataset.graph.order(),'\nEdge Number:',
           network_dataset.graph.size())
-    print('Using Random Partitioning Method...\nServer Number: 128\nVirtual Primary Copy Number:',vp_number,
+    print('Using Random Partitioning Method...\nServer Number:',len(server_list),'\nVirtual Primary Copy Number:',vp_number,
           '\nWrite Frequency of Nodes: 1')
     start=time.time()
     m = RandomP(server_list, network_dataset,node_list)
@@ -36,5 +36,8 @@ def main():
     end=time.time()
     print('Random Partitioning Time:',end-start,'seconds')
     m.compute_inter_sever_cost()
+    path = RANDOM_GRAPH_PATH
+    m.save_all(path)
+
 
 main()
