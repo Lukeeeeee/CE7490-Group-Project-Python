f085fc5476bf68c670b636490e6fd177b4bffa86
plusmultiply <527441520@qq.com>
2018-10-29 22:55:07 +0800

    Add files via upload

--- test/sparTest.py
+++ test/sparTest.py
@@ -1,6 +1,6 @@
-from src.algo.baselines.SPAR.spar_sample import test_spar_sample
+from src.algo.baselines.SPAR.spar_sample import spar_experiments
 from dataset import DATASET_PATH
 import os
 
 if __name__ == '__main__':
-    test_spar_sample(dataset_file=os.path.join(DATASET_PATH, 'AmazonSample.txt'))
+    spar_experiments()
