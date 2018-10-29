0467b90c0c8fb611ab5907498e94e52a4bf5ffe0
Lukeeeeee <lukedong123@gmail.com>
2018-10-29 22:55:37 +0800

    clean the code

---
+++ test/offline_algo_test.py
@@ -0,0 +1,215 @@
+import os
+import sys
+
+CURRENT_PATH = os.path.dirname(os.path.realpath(__file__))
+sys.path.append(CURRENT_PATH)
+PAR_PATH = os.path.abspath(os.path.join(CURRENT_PATH, os.pardir))
+sys.path.append(PAR_PATH)
+import logging
+from log import LOG_PATH
+
+from src.algo.offlineAlgo import OfflineAlgo
+import networkx as nx
+from src.dataset import Dataset
+from src.server.server import Server
+from src.constant import Constant
+import time
+from src.algo.operation import Operation as op
+
+
+def print_graph(server_list):
+    for server in server_list:
+        log_str = "Server: %d, load: %d" % (server.id, server.get_load())
+        logging.info(log_str)
+        print(log_str)
+        for node in server.graph.nodes:
+            log_str = "%d: node type %s, write freq %f" % (node, server.graph.nodes[node]['node_type'],
+                                                           server.graph.nodes[node]['write_freq'])
+            logging.info(log_str)
+            print(log_str)
+
+
+def main(dataset='amazon', part_flag=0.01, log_path_end='', tmp_log_flag=False):
+    log_path = LOG_PATH
+    log_path = os.path.join(log_path, '%s_%s_%s_%s_%s' % (
+        time.strftime("%Y-%m-%d_%H-%M-%S"), 'offline', dataset, str(part_flag), log_path_end))
+    if not os.path.exists(log_path):
+        os.mkdir(log_path)
+    logging.basicConfig(level=logging.DEBUG,
+                        filename=log_path + '/log',
+                        filemode='w')
+    Constant().log_out()
+    data = Dataset(dataset_str=dataset, part_flag=part_flag)
+
+    server_list = [Server(serer_id=i) for i in range(Constant.SERVER_NUMBER)]
+    algo = OfflineAlgo(server_list=server_list, network_dataset=data)
+    node_list = list(data.graph.nodes)
+    node_len = len(node_list)
+    for i in range(node_len):
+        n = node_list[i]
+        log_str = "(%d/%d) Adding node: %d" % (i, node_len, n)
+        logging.info(log_str)
+        print(log_str)
+        algo.add_new_primary_node(node_id=n, write_freq=Constant.WRITE_FREQ)
+    print_graph(server_list)
+    op.validate_result(dataset_g=algo.network_dataset.graph,
+                       server_g_list=[x.graph for x in algo.server_list],
+                       load_differ=Constant.MAX_LOAD_DIFFERENCE_AMONG_SERVER,
+                       virtual_copy_number=Constant.LEAST_VIRTUAL_PRIMARY_COPY_NUMBER)
+    log_str = 'Inter Server cost is %f' % algo.compute_inter_server_cost()
+    print(log_str)
+    logging.info(log_str)
+
+    print("Running relocation process-------------")
+    logging.info("Running relocation process-------------")
+    algo.node_relocation_process()
+    log_str = 'Inter Server cost is %f' % algo.compute_inter_server_cost()
+    print(log_str)
+    logging.info(log_str)
+    print_graph(server_list)
+    op.validate_result(dataset_g=algo.network_dataset.graph,
+                       server_g_list=[x.graph for x in algo.server_list],
+                       load_differ=Constant.MAX_LOAD_DIFFERENCE_AMONG_SERVER,
+                       virtual_copy_number=Constant.LEAST_VIRTUAL_PRIMARY_COPY_NUMBER)
+    print("Init merge process-------------")
+    logging.info("Init merge process-------------")
+    algo.init_merge_process()
+    print_graph(server_list)
+    op.validate_result(dataset_g=algo.network_dataset.graph,
+                       server_g_list=[x.graph for x in algo.server_list],
+                       load_differ=Constant.MAX_LOAD_DIFFERENCE_AMONG_SERVER,
+                       virtual_copy_number=Constant.LEAST_VIRTUAL_PRIMARY_COPY_NUMBER)
+    print("Start merge process-------------")
+    logging.info("Start merge process-------------")
+    algo.start_merge_process()
+    print_graph(server_list)
+    log_str = 'Inter Server cost is %f' % algo.compute_inter_server_cost()
+    print(log_str)
+    logging.info(log_str)
+    op.validate_result(dataset_g=algo.network_dataset.graph,
+                       server_g_list=[x.graph for x in algo.server_list],
+                       load_differ=Constant.MAX_LOAD_DIFFERENCE_AMONG_SERVER,
+                       virtual_copy_number=Constant.LEAST_VIRTUAL_PRIMARY_COPY_NUMBER)
+    print("Init Group Swap process-------------")
+    logging.info("Init Group Swap process-------------")
+    algo.init_group_swap_process(algo)
+    print_graph(server_list)
+    op.validate_result(dataset_g=algo.network_dataset.graph,
+                       server_g_list=[x.graph for x in algo.server_list],
+                       load_differ=Constant.MAX_LOAD_DIFFERENCE_AMONG_SERVER,
+                       virtual_copy_number=Constant.LEAST_VIRTUAL_PRIMARY_COPY_NUMBER)
+    print("Virtual Primary Copy Swap process-------------")
+    logging.info("Virtual Swap Copy Swap process-------------")
+    algo.virtual_primary_copy_swap()
+    print_graph(server_list)
+    log_str = 'Inter Server cost is %f' % algo.compute_inter_server_cost()
+    print(log_str)
+    logging.info(log_str)
+    op.validate_result(dataset_g=algo.network_dataset.graph,
+                       server_g_list=[x.graph for x in algo.server_list],
+                       load_differ=Constant.MAX_LOAD_DIFFERENCE_AMONG_SERVER,
+                       virtual_copy_number=Constant.LEAST_VIRTUAL_PRIMARY_COPY_NUMBER)
+
+    print("Remove redundant replica-------------")
+    logging.info("Remove redundant replica-------------")
+    algo.remove_unnecessary_replica()
+    log_str = 'Inter Server cost is %f' % algo.compute_inter_server_cost()
+    print(log_str)
+    logging.info(log_str)
+
+    algo.save_all(path=log_path)
+    g, server = op.load_log(log_path)
+    op.validate_result(dataset_g=g,
+                       server_g_list=server,
+                       load_differ=Constant.MAX_LOAD_DIFFERENCE_AMONG_SERVER,
+                       virtual_copy_number=Constant.LEAST_VIRTUAL_PRIMARY_COPY_NUMBER)
+    log_str = 'Inter Server cost is %f' % algo.compute_inter_server_cost()
+    print(log_str)
+    logging.info(log_str)
+    print(log_path)
+    for handler in logging.root.handlers[:]:
+        logging.root.removeHandler(handler)
+
+
+def fig3():
+    Constant.LEAST_VIRTUAL_PRIMARY_COPY_NUMBER = 0
+    server_c = [2, 4, 8, 16, 32, 64, 96, 128, 256, 512]
+    server_c = [2, 4, 8, 16, 32, 64, 96, 128]
+    server_c = [2, 4, 8, 16]
+    server_c = [32, 64, 96, 128]
+    server_c = [2, 4, 6, 8]
+    for s in server_c:
+        Constant.SERVER_NUMBER = s
+        main(dataset='twitters2',
+             part_flag=0.5,
+             log_path_end='v2_fig_3_server_%d_vir_copy_%d' % (
+                 Constant.SERVER_NUMBER, Constant.LEAST_VIRTUAL_PRIMARY_COPY_NUMBER))
+
+
+def fig8():
+    Constant.LEAST_VIRTUAL_PRIMARY_COPY_NUMBER = 0
+    Constant.SERVER_NUMBER = 128
+    res = [0.02, 0.05, 0.08, 0.10]
+    for r in res:
+        main(dataset='facebook',
+             part_flag=r,
+             log_path_end='v2_fig_8_server_%d_vir_copy_%d' % (
+                 Constant.SERVER_NUMBER, Constant.LEAST_VIRTUAL_PRIMARY_COPY_NUMBER))
+
+
+def fig9():
+    Constant.LEAST_VIRTUAL_PRIMARY_COPY_NUMBER = 3
+    Constant.SERVER_NUMBER = 128
+    res = [0.02, 0.05, 0.08, 0.10]
+    for r in res:
+        main(dataset='facebook',
+             part_flag=r,
+             log_path_end='v2_fig_9_server_%d_vir_copy_%d' % (
+                 Constant.SERVER_NUMBER, Constant.LEAST_VIRTUAL_PRIMARY_COPY_NUMBER))
+
+
+def fig11():
+    dataset = ['twitters1', 'twitters2', 'amazons', 'p2pgnutella', 'facebook']
+    Constant.LEAST_VIRTUAL_PRIMARY_COPY_NUMBER = 3
+    Constant.SERVER_NUMBER = 128
+    for r in dataset:
+        main(dataset=r,
+             part_flag=0.1,
+             log_path_end='v2_fig_11_server_%d_vir_copy_%d' % (
+                 Constant.SERVER_NUMBER, Constant.LEAST_VIRTUAL_PRIMARY_COPY_NUMBER))
+
+
+def fig15_16():
+    Constant.LEAST_VIRTUAL_PRIMARY_COPY_NUMBER = 0
+    Constant.SERVER_NUMBER = 10
+    # for r in dataset:
+    #     main(dataset=r,
+    #          part_flag=0.1,
+    #          log_path_end='v2_fig_15_16_server_%d_vir_copy_%d' % (
+    #              Constant.SERVER_NUMBER, Constant.LEAST_VIRTUAL_PRIMARY_COPY_NUMBER))
+    main(dataset='facebook',
+         part_flag=0.01,
+         log_path_end='v2_fig_15_16_server_%d_vir_copy_%d' % (
+             Constant.SERVER_NUMBER, Constant.LEAST_VIRTUAL_PRIMARY_COPY_NUMBER))
+
+
+def fig19():
+    Constant.SERVER_NUMBER = 8
+    res = [0, 1, 2, 3, 4, 6, 7]
+    for r in res:
+        Constant.LEAST_VIRTUAL_PRIMARY_COPY_NUMBER = r
+        main(dataset='amazons',
+             part_flag=0.1,
+             log_path_end='v2_fig_19_server_%d_vir_copy_%d' % (
+                 Constant.SERVER_NUMBER, Constant.LEAST_VIRTUAL_PRIMARY_COPY_NUMBER))
+
+
+if __name__ == '__main__':
+    # part flag is to decide the percentage of data to be used due to the limitation of our computing resources
+    # Each experiments will have a log folder at /CE7490-Group-Project-Python/log/
+    # log_path_end will be append to the log path
+    main(part_flag=0.01,
+         dataset='amazons',
+         log_path_end='debug')
+    # You can run experiments corresponding to different figures of the original paper by:
+    # fig3()
---
+++ test/unit_test_offline.py
@@ -0,0 +1,230 @@
+import unittest
+import networkx as nx
+from src.algo.offlineAlgo import OfflineAlgo
+from src.dataset import Dataset
+from src.server.server import Server
+from src.constant import Constant
+from src.algo.operation import Operation
+
+
+def create_algo(server_count=4, node_count=10):
+    data = Dataset(dataset_str='facebook')
+    data.graph = nx.Graph()
+    for i in range(node_count):
+        data.graph.add_node(i)
+    server_list = [Server(serer_id=i) for i in range(server_count)]
+    algo = OfflineAlgo(server_list=server_list, network_dataset=data)
+    return algo
+
+
+class TestOfflineAlgo(unittest.TestCase):
+    # this is a simple unit test for offline algorithm when we develop the code.
+    def test_basic_assign_new_node(self):
+        algo = create_algo(node_count=4, server_count=10)
+        algo.network_dataset.graph.add_edge(0, 1)
+        algo._add_node_to_server(node_id=0, node_type=Constant.PRIMARY_COPY, write_freq=10.0,
+                                 server=algo.server_list[0])
+        algo._add_node_to_server(node_id=1, node_type=Constant.PRIMARY_COPY, write_freq=10.0,
+                                 server=algo.server_list[1])
+        self.assertTrue(algo.server_list[0].has_node(node_id=0, node_type=Constant.PRIMARY_COPY))
+        self.assertTrue(algo.server_list[1].has_node(node_id=0, node_type=Constant.NON_PRIMARY_COPY) or
+                        algo.server_list[1].has_node(node_id=0, node_type=Constant.VIRTUAL_PRIMARY_COPY))
+
+        self.assertTrue(algo.server_list[1].has_node(node_id=1, node_type=Constant.PRIMARY_COPY))
+        self.assertTrue(algo.server_list[0].has_node(node_id=1, node_type=Constant.NON_PRIMARY_COPY) or
+                        algo.server_list[0].has_node(node_id=1, node_type=Constant.VIRTUAL_PRIMARY_COPY))
+
+        algo.get_node_with_id(1).add_non_primary_copy(target_server=algo.server_list[9])
+        self.assertTrue(algo.server_list[9].has_node(node_id=1, node_type=Constant.NON_PRIMARY_COPY))
+
+        Operation.remove_redundant_replica_of_node(node=algo.get_node_with_id(1), algo=algo)
+        self.assertFalse(algo.server_list[9].has_node(node_id=1, node_type=Constant.NON_PRIMARY_COPY))
+
+    def test_node_relation_func(self):
+        algo = create_algo()
+        algo.network_dataset.graph.add_edge(0, 1)
+        algo._add_node_to_server(node_id=0, node_type=Constant.PRIMARY_COPY, write_freq=10.0,
+                                 server=algo.server_list[0])
+        algo._add_node_to_server(node_id=1, node_type=Constant.PRIMARY_COPY, write_freq=10.0,
+                                 server=algo.server_list[0])
+        self.assertTrue(algo.is_ssn_with(0, 1))
+        self.assertTrue(algo.is_ssn_with(1, 0))
+        self.assertTrue(algo.is_pssn_with(0, 1))
+        self.assertTrue(algo.is_pssn_with(1, 0))
+
+        self.assertFalse(algo.is_pdsn_with(0, 1))
+        self.assertFalse(algo.is_pdsn_with(1, 0))
+        self.assertFalse(algo.is_dsn_with(0, 1))
+        self.assertFalse(algo.is_dsn_with(1, 0))
+
+        algo.network_dataset.graph.add_edge(0, 2)
+
+        algo._add_node_to_server(node_id=2, node_type=Constant.PRIMARY_COPY, write_freq=10.0,
+                                 server=algo.server_list[1])
+        self.assertFalse(algo.is_ssn_with(0, 2))
+        self.assertFalse(algo.is_ssn_with(2, 0))
+        self.assertFalse(algo.is_pssn_with(0, 2))
+        self.assertFalse(algo.is_pssn_with(2, 0))
+
+        self.assertFalse(algo.is_pssn_with(0, 1))
+        self.assertTrue(algo.is_pssn_with(1, 0))
+
+        self.assertTrue(algo.is_dsn_with(0, 2))
+        self.assertTrue(algo.is_dsn_with(2, 0))
+        self.assertTrue(algo.is_pdsn_with(0, 2))
+        self.assertTrue(algo.is_pdsn_with(2, 0))
+
+    def test_node_move(self):
+        algo = create_algo(server_count=4, node_count=10)
+        algo.network_dataset.graph.add_edge(0, 1)
+        algo._add_node_to_server(node_id=0, node_type=Constant.PRIMARY_COPY, write_freq=10.0,
+                                 server=algo.server_list[0])
+        algo._add_node_to_server(node_id=1, node_type=Constant.PRIMARY_COPY, write_freq=10.0,
+                                 server=algo.server_list[1])
+        self.assertTrue(algo.server_list[0].has_node(0, node_type=Constant.PRIMARY_COPY))
+        self.assertTrue(algo.server_list[1].has_node(0, node_type=Constant.NON_PRIMARY_COPY) or
+                        algo.server_list[1].has_node(0, node_type=Constant.VIRTUAL_PRIMARY_COPY))
+
+        Operation.move_node_to_server(node=algo.get_node_with_id(0), target_server=algo.server_list[1], algo=algo)
+
+        self.assertFalse(algo.server_list[0].has_node(0, node_type=Constant.PRIMARY_COPY))
+
+        self.assertTrue(algo.server_list[1].has_node(0, node_type=Constant.PRIMARY_COPY))
+        self.assertFalse(algo.server_list[1].has_node(0, node_type=Constant.NON_PRIMARY_COPY))
+        self.assertFalse(algo.server_list[1].has_node(0, node_type=Constant.VIRTUAL_PRIMARY_COPY))
+
+    def test_relocate_process(self):
+        data = Dataset(dataset_str='facebook')
+        data.graph = nx.Graph()
+        for i in range(10):
+            data.graph.add_node(i)
+        data.graph.add_edge(0, 1)
+        data.graph.add_edge(0, 2)
+        data.graph.add_edge(0, 3)
+        data.graph.add_edge(0, 4)
+        server_list = [Server(serer_id=i) for i in range(8)]
+        algo = OfflineAlgo(server_list=server_list, network_dataset=data)
+        node_list = list(data.graph.nodes)
+        node_len = len(node_list)
+        for i in range(node_len):
+            n = node_list[i]
+            algo.add_new_primary_node(node_id=n, write_freq=Constant.WRITE_FREQ)
+        algo.node_relocation_process()
+        # self.assertEqual(algo.compute_inter_server_cost(), 0)
+
+    def test_merge_process(self):
+        data = Dataset(dataset_str='facebook')
+        data.graph = nx.Graph()
+        for i in range(10):
+            data.graph.add_node(i)
+        data.graph.add_edge(0, 1)
+        data.graph.add_edge(0, 2)
+        data.graph.add_edge(0, 3)
+        data.graph.add_edge(0, 4)
+        server_list = [Server(serer_id=i) for i in range(8)]
+        algo = OfflineAlgo(server_list=server_list, network_dataset=data)
+        node_list = list(data.graph.nodes)
+        node_len = len(node_list)
+        for i in range(node_len):
+            n = node_list[i]
+            algo.add_new_primary_node(node_id=n, write_freq=Constant.WRITE_FREQ)
+        algo.init_merge_process()
+
+        for i in range(0, len(algo.merged_node_list)):
+            m_node = algo.merged_node_list[i]
+            if m_node.id == 0:
+                self.assertEqual(m_node.internal_connection, 0)
+                self.assertEqual(m_node.external_connection, 4)
+            elif m_node.id in [1, 2, 3, 4]:
+                self.assertEqual(m_node.internal_connection, 0)
+                self.assertEqual(m_node.external_connection, 1)
+            else:
+                self.assertEqual(m_node.internal_connection, 0)
+                self.assertEqual(m_node.external_connection, 0)
+        node_count_list = []
+        for m_node in algo.merged_node_list:
+            node_count_list += m_node.node_id_list
+        node_count_list.sort()
+        self.assertEqual(node_count_list, [i for i in range(10)])
+        for i in range(1, len(algo.merged_node_list)):
+            algo.merged_node_list[0]._add_node(algo.merged_node_list[i], algo=algo, remove_flag=False)
+        node_count_list = algo.merged_node_list[0].node_id_list
+        node_count_list.sort()
+        self.assertEqual(node_count_list, [i for i in range(10)])
+        self.assertEqual(algo.merged_node_list[0].external_connection, 0)
+        self.assertEqual(algo.merged_node_list[0].internal_connection, 4)
+        self.assertEqual(algo.merged_node_list[0].node_count, 10)
+
+    def test_merged_node_swap_process(self):
+
+        data = Dataset(dataset_str='facebook')
+        data.graph = nx.Graph()
+        for i in range(10):
+            data.graph.add_node(i)
+        data.graph.add_edge(0, 1)
+        data.graph.add_edge(0, 2)
+        data.graph.add_edge(0, 3)
+        data.graph.add_edge(0, 4)
+        server_list = [Server(serer_id=i) for i in range(8)]
+        algo = OfflineAlgo(server_list=server_list, network_dataset=data)
+        node_list = list(data.graph.nodes)
+        node_len = len(node_list)
+        for i in range(node_len):
+            n = node_list[i]
+            algo.add_new_primary_node(node_id=n, write_freq=Constant.WRITE_FREQ)
+        algo.init_merge_process()
+        algo.start_merge_process()
+        node_count_list = []
+        for m_node in algo.merged_node_list:
+            node_count_list += m_node.node_id_list
+        node_count_list.sort()
+        self.assertEqual(node_count_list, [i for i in range(10)])
+
+    def test_virtual_primary_copy_process(self):
+        data = Dataset(dataset_str='facebook')
+        data.graph = nx.Graph()
+        data.graph.add_node(0)
+        server_list = [Server(serer_id=i) for i in range(2)]
+        Constant.LEAST_VIRTUAL_PRIMARY_COPY_NUMBER = 1
+        algo = OfflineAlgo(server_list=server_list, network_dataset=data)
+        node_list = list(data.graph.nodes)
+        node_len = len(node_list)
+        for i in range(node_len):
+            n = node_list[i]
+            algo.add_new_primary_node(node_id=n, write_freq=Constant.WRITE_FREQ)
+        self.assertEqual(len(algo.node_list), 1)
+        self.assertEqual(algo.node_list[0].id, 0)
+        self.assertEqual(algo.node_list[0].virtual_primary_copy_server_list[0].id, 1 - algo.node_list[0].server.id)
+        self.assertEqual(len(algo.node_list[0].virtual_primary_copy_server_list), 1)
+        data.graph.add_edge(0, 1)
+        algo.server_list.append(Server(2))
+        algo.server_list.append(Server(3))
+
+        algo._add_node_to_server(node_id=1, node_type=Constant.PRIMARY_COPY, write_freq=10.0,
+                                 server=algo.server_list[2])
+
+        # algo.virtual_primary_copy_swap()
+        for vir_server in algo.node_list[0].virtual_primary_copy_server_list:
+            if vir_server.id != algo.node_list[1].virtual_primary_copy_server_list[0].id:
+                tmp_server_1_id = vir_server
+                tmp_server_2_id = algo.node_list[1].virtual_primary_copy_server_list[0]
+
+                Operation.swap_virtual_primary_copy(s_node=algo.node_list[0],
+                                                    t_node=algo.node_list[1],
+                                                    s_server=vir_server,
+                                                    t_server=algo.node_list[1].virtual_primary_copy_server_list[0],
+                                                    algo=algo)
+                break
+        self.assertTrue(tmp_server_1_id.has_node(algo.node_list[1].id, node_type=Constant.VIRTUAL_PRIMARY_COPY))
+        self.assertTrue(tmp_server_2_id.has_node(algo.node_list[0].id, node_type=Constant.VIRTUAL_PRIMARY_COPY))
+        self.assertTrue(tmp_server_2_id in algo.node_list[0].virtual_primary_copy_server_list)
+        self.assertTrue(tmp_server_1_id in algo.node_list[1].virtual_primary_copy_server_list)
+
+    def test_remove_util(self):
+        server_list = [Server(serer_id=i) for i in range(10)]
+        server_list.remove(server_list[1])
+        self.assertEqual(len(server_list), 9)
+
+
+if __name__ == '__main__':
+    unittest.main()
